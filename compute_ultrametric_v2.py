2ec178bc52af24f805be120f1b42963a04872104
Thiago <tmelo.mat@gmail.com>
2019-06-21 18:37:10 -0300

    Add files via upload

--- compute_ultrametric_v2.py
+++ compute_ultrametric_v2.py
@@ -15,53 +15,6 @@ def loop_ultrametric_inner_state():
             ultrametric_inner_state(state)
 
 
-def max_prod(mat1, mat2=''):
-    """ max_prod function
-     input: mat1, mat2 square matrices of same order
-    output: square matrix with (i,j) entry equals to
-            min_max(mat1, mat2, i, j) """
-    N = len(mat1)
-    #tmp = np.zeros((n, n), dtype=float)
-    '''
-    for i in range(n):
-        print 'max_prod: ', i
-        for j in range(i, n):
-            tmp[i, j] = min_max(mat1, mat2, i, j)
-            tmp[j, i] = min_max(mat2, mat1, j, i)
-    '''
-    tmp = np.asarray([min_max(mat1, p) for p in pairs])
-    tri = np.zeros((N, N), dtype=int)
-    tri[np.triu_indices(N, 1)] = tmp
-    tri[np.tril_indices(N, -1)] = tmp
-    #np.savetxt('kk.csv', tri, fmt='%d', delimiter=',')
-    return tri
-
-
-def min_max(mat, p):
-    """ min_max function
-     input: mat1, mat2 square matrices of same order
-            i, j positive integers
-    output: the minimum of the maximum pointwise vector
-            from mat1(row i) and mat2(col j) """
-    i, j = p[0], p[1]
-    return min(np.maximum(mat[i, :], mat[:, j]))
-
-
-def power(mat):
-    """ power function
-    computes the iterated max_prod
-     input: mat n-squared matrix
-    output: mat**(n-1) """
-    tmp = mat
-    n = len(tmp)
-    k = 1
-    while 2 ** (k - 1) < n:
-        print k,2**k, n
-        tmp = max_prod(tmp)
-        k += 1
-    return tmp
-
-
 def u_nr(mat, output_file='uNR.csv'):
     """ non-reciprocal ultra metric function
      input: mat squared matrix
@@ -70,9 +23,6 @@ def u_nr(mat, output_file='uNR.csv'):
     print 'Computing non-reciprocal ultra metric. Wait... ',
     dj_mat = dj(mat)
     tmp = np.maximum(dj_mat, dj_mat.T)
-    #tmp1 = power(mat)
-    #tmp2 = tmp1.T
-    #tmp = np.maximum(tmp1, tmp2)
     np.savetxt('ultra_metric/' + output_file.replace(' ', '_'), tmp, fmt='%f',
                delimiter=',')
     print 'saved in %s' % output_file.replace(' ', '_')
@@ -145,8 +95,8 @@ migration_matrix_file = 'migration_matrix.csv'
 states = np.loadtxt('prepare_data_states_num_counties.csv', dtype=str,
                     delimiter=',')[:, 0]
 
-# loop_ultrametric_inner_state()
+loop_ultrametric_inner_state()
 
 """ This function takes hours to finish. """
-# ultrametric_national()
+ultrametric_national()
 
